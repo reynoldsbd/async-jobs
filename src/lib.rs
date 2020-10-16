1a433e64c67de1374d585108f0c8aad53bac1ba0
Bobby Reynolds <bobby@reynoldsbd.net>
2020-10-15 23:35:44 -0700

    ready queue

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 //! To run your tasks, pass an instance of your `Job` to the `Scheduler::run` method.
 
 use std::collections::HashSet;
+use std::mem;
 
 use async_trait::async_trait;
 
@@ -33,17 +34,83 @@ pub enum Error<E> {
     Cycle,
 
     /// One or more jobs failed
-    Fail(E),
+    Fail(Vec<E>),
+}
+
+/// Tracks the state of a scheduled job
+enum JobState<J: Job> {
+
+    /// Job has not yet started running
+    Pending(J),
+
+    /// Job is actively executing
+    Running,
+
+    /// Job failed execution
+    Failed(J::Error),
+
+    /// Job finished execution successfully
+    Succeeded,
+}
+
+impl<J: Job> JobState<J> {
+
+    /// Returns whether or not this state equals `JobState::Succeeded`
+    fn succeeded(&self) -> bool {
+        match self {
+            JobState::Succeeded => true,
+            _                   => false,
+        }
+    }
+}
+
+/// Bookkeeping information about a scheduled job
+struct ScheduleEntry<J: Job> {
+
+    /// Current state of corresponding job
+    state: JobState<J>,
+
+    /// List of jobs upon which this job depends
+    dependencies: Vec<usize>,
+
+    /// List of jobs which depend on this job
+    dependents: Vec<usize>,
+}
+
+impl<J: Job> ScheduleEntry<J> {
+
+    /// Returns a reference to the job held by this entry
+    ///
+    /// # Panics
+    ///
+    /// Panics if this is any other state besides `Pending`
+    fn job(&self) -> &J {
+        if let JobState::Pending(ref j) = self.state {
+            j
+        } else {
+            panic!("unexpected job state")
+        }
+    }
+}
+
+impl<J: Job> From<J> for ScheduleEntry<J> {
+    fn from(job: J) -> Self {
+        Self {
+            state: JobState::Pending(job),
+            dependencies: Default::default(),
+            dependents: Default::default(),
+        }
+    }
 }
 
 /// Data structure used to schedule execution of dependent jobs
-struct Schedule<J> {
+struct Schedule<J: Job> {
 
     /// List of jobs which comprise this schedule
-    jobs: Vec<J>,
+    jobs: Vec<ScheduleEntry<J>>,
 
-    /// List of indices (into `self.jobs`) in order of execution
-    queue: Vec<usize>,
+    /// List of jobs that are ready to execute
+    ready: Vec<usize>,
 }
 
 impl<J: Job> Schedule<J> {
@@ -53,7 +120,7 @@ impl<J: Job> Schedule<J> {
 
         let mut sched = Self {
             jobs: Default::default(),
-            queue: Default::default(),
+            ready: Default::default(),
         };
 
         let mut ancestors = Default::default();
@@ -69,24 +136,60 @@ impl<J: Job> Schedule<J> {
     fn add_job(&mut self, job: J, ancestors: &mut HashSet<usize>) -> Result<usize, Error<J::Error>> {
 
         let job_idx = self.jobs.len();
-        self.jobs.push(job);
+        self.jobs.push(job.into());
 
         debug_assert!(ancestors.insert(job_idx));
 
-        for dep in self.jobs[job_idx].dependencies() {
-            if let Some(dep_idx) = self.jobs.iter().position(|j| *j == dep) {
-                if ancestors.contains(&dep_idx) {
-                    return Err(Error::Cycle)
-                }
-            } else {
-                self.add_job(dep, ancestors)?;
-            }
+        for dep in self.jobs[job_idx].job().dependencies() {
+
+            let dep_idx = self.jobs.iter().position(|j| j.job() == &dep)
+                .map(|i| if ancestors.contains(&i) { Err(Error::Cycle) } else { Ok(i) })
+                .unwrap_or_else(|| self.add_job(dep, ancestors))?;
+
+            self.jobs[dep_idx].dependents.push(job_idx);
+            self.jobs[job_idx].dependencies.push(dep_idx);
         }
 
         debug_assert!(ancestors.remove(&job_idx));
 
-        self.queue.push(job_idx);
+        if self.jobs[job_idx].dependencies.len() == 0 {
+            self.ready.push(job_idx);
+        }
+
         Ok(job_idx)
+    }
+
+    /// Returns the next job from the ready queue, along with its index
+    fn next_job(&mut self) -> Option<(J, usize)> {
+
+        if self.ready.len() == 0 {
+            return None;
+        }
+
+        let idx = self.ready.remove(0);
+        let state = mem::replace(&mut self.jobs[idx].state, JobState::Running);
+
+        if let JobState::Pending(job) = state {
+            Some((job, idx))
+        } else {
+            panic!("unexpected job status")
+        }
+    }
+
+    /// Marks a job as completed and updates the ready queue with any new jobs that
+    /// are now ready to execute as a result.
+    fn mark_complete(&mut self, job_idx: usize, err: Option<J::Error>) {
+
+        self.jobs[job_idx].state = err.map(|e| JobState::Failed(e))
+            .unwrap_or(JobState::Succeeded);
+
+        for dep_idx in &self.jobs[job_idx].dependents {
+            let is_ready = self.jobs[*dep_idx].dependencies.iter()
+                .all(|i| self.jobs[*i].state.succeeded());
+            if is_ready {
+                self.ready.push(*dep_idx);
+            }
+        }
     }
 }
 
@@ -101,14 +204,25 @@ impl Scheduler {
     /// Executes `job` and its dependencies on this scheduler
     pub async fn run<J: Job>(&self, job: J) -> Result<(), Error<J::Error>> {
 
-        let sched = Schedule::new(job)?;
+        let mut sched = Schedule::new(job)?;
 
-        for i in sched.queue {
-            sched.jobs[i].run().await
-                .map_err(|e| Error::Fail(e))?;
+        while let Some((job, idx)) = sched.next_job() {
+            let res = job.run().await;
+            sched.mark_complete(idx, res.err());
         }
 
-        Ok(())
+        let mut errs = vec![];
+        for job in sched.jobs {
+            if let JobState::Failed(err) = job.state {
+                errs.push(err);
+            }
+        }
+
+        if errs.len() > 0 {
+            Err(Error::Fail(errs))
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -214,7 +328,7 @@ mod tests {
             (false, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(0)));
+        assert_eq!(err, Some(Error::Fail(vec![0])));
         assert_eq!(trace[0], Some(0));
     }
 
@@ -239,7 +353,7 @@ mod tests {
             (false, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(1)));
+        assert_eq!(err, Some(Error::Fail(vec![1])));
         assert_eq!(trace[0], None);
         assert_eq!(trace[1], Some(0));
     }
@@ -252,7 +366,7 @@ mod tests {
             (true, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(0)));
+        assert_eq!(err, Some(Error::Fail(vec![0])));
         assert_eq!(trace[0], Some(1));
         assert_eq!(trace[1], Some(0));
     }
@@ -281,7 +395,7 @@ mod tests {
             (false, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(2)));
+        assert_eq!(err, Some(Error::Fail(vec![2])));
         assert_eq!(trace[0], None);
         // job 1 may or may not be updated
         assert!(trace[2].is_some());
@@ -311,7 +425,7 @@ mod tests {
             (false, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(2)));
+        assert_eq!(err, Some(Error::Fail(vec![2])));
         assert_eq!(trace[0], None);
         assert_eq!(trace[1], None);
         assert_eq!(trace[2], Some(0));
@@ -326,7 +440,7 @@ mod tests {
             (true, vec![]),
         ]).await;
 
-        assert_eq!(err, Some(Error::Fail(1)));
+        assert_eq!(err, Some(Error::Fail(vec![1])));
         assert_eq!(trace[0], None);
         assert_eq!(trace[1], Some(1));
         assert_eq!(trace[2], Some(0));
